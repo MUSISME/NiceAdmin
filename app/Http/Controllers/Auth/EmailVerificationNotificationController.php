f75bf460e075351e97aa7444716918a812da0564
Ilias Daniel <76595153+ilias1403@users.noreply.github.com>
2025-01-25 10:00:18 +0800

    verify email

---
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class EmailVerificationNotificationController extends Controller
+{
+    /**
+     * Send a new email verification notification.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(route('dashboard', absolute: false));
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()
+            ->with('status', 'success')
+            ->with('message', 'Successful sent verification to your email!');
+    }
+}
